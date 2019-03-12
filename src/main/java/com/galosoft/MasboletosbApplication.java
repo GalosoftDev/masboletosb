cf4d14b5cf5a62b583420cb194603526dd53246c
bruno <bruno@bruno>
2019-03-12 10:44:19 -0600

    First Commit config masboletosb app with springboot

---
+++ src/main/java/com/galosoft/MasboletosbApplication.java
@@ -0,0 +1,13 @@
+package com.galosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasboletosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasboletosbApplication.class, args);
+	}
+
+}
